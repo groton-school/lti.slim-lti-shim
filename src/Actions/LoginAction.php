10e42a627d12497647e7afcf30abaaecff93b308
battis <sbattis@groton.org>
2025-03-24 19:24:57 +0000

    refactor(slim-lti-shim): rewrite to match packback docs

--- src/Actions/LoginAction.php
+++ src/Actions/LoginAction.php
@@ -12,14 +12,12 @@ class LoginAction extends LTIAction
 {
     protected function action(): Response
     {
-        error_log(var_export($this->request->getParsedBody(), true));
         $login = LtiOidcLogin::new($this->database, $this->cache, $this->cookie);
         try {
             $redirect = $login->getRedirectUrl($this->projectUrl . '/lti/launch', $this->request->getParsedBody());
         } catch (OidcException $e) {
-            error_log(var_export($e, true));
+            var_dump($e);
         }
-        error_log("Attempting redirect to $redirect");
         return $this->response->withHeader('Location', $redirect);
     }
 }
