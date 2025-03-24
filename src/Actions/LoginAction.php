ae6c05fb9f31d049590eb283e12e0dfe1ccff261
Seth Battis <sbattis@groton.org>
2025-03-24 14:34:16 -0400

    refactor(slim-lti-shim): rewrite to match packback docs
    https://github.com/packbackbooks/lti-1-3-php-library/wiki/Endpoints-and-Handling-Requests#open-id-connect-login-request

--- src/Actions/LoginAction.php
+++ src/Actions/LoginAction.php
@@ -5,24 +5,19 @@ declare(strict_types=1);
 namespace GrotonSchool\Slim\LTI\Actions;
 
 use Packback\Lti1p3\LtiOidcLogin;
+use Packback\Lti1p3\OidcException;
 use Psr\Http\Message\ResponseInterface as Response;
 
 class LoginAction extends LTIAction
 {
     protected function action(): Response
     {
-        return $this->response
-            ->withStatus(302)
-            ->withHeader(
-                'Location',
-                LtiOidcLogin::new(
-                    $this->database,
-                    $this->cache,
-                    $this->cookie
-                )->getRedirectUrl(
-                    $this->projectUrl . '/lti/launch',
-                    $this->request->getParsedBody()
-                )
-            );
+        $login = LtiOidcLogin::new($this->database, $this->cache, $this->cookie);
+        try {
+            $redirect = $login->getRedirectUrl($this->projectUrl . '/lti/launch', $this->request->getParsedBody());
+        } catch (OidcException $e) {
+            var_dump($e);
+        }
+        return $this->response->withHeader('Location', $redirect);
     }
 }
