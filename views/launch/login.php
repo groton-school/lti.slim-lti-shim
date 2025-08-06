28455f7094bb9a08dbf52798dcb48cffad93a812
Seth Battis <sbattis@groton.org>
2025-08-06 15:54:23 -0400

    refactor: add viewport config to views for mobile

--- views/launch/login.php
+++ views/launch/login.php
@@ -2,6 +2,8 @@
 <html lang="en">
 
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>LTI Launch</title>
     <style type="text/css">
         body {
